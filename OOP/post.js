f03ff161275da920095412f7cb0c90dad4e6fdcc
Sanyi Tóth <sandor.toth@hotmail.com>
2021-03-06 11:38:10 +0100

    post class property and method created

--- OOP/post.js
+++ OOP/post.js
@@ -1,5 +1,16 @@
-export class Post{
+export class Post {
+    title;
+    body;
+    #id;
+
+    constructor(title, body, id) {
+        this.title = title;
+        this.body = body;
+        this.#id = id;
+    }
 
 
-
+    getPreview(length) {
+        return this.body.slice(0, length);
+    }
 }
