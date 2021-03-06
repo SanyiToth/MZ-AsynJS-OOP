07342af273367c0a84212467b05fb01f3db15d9b
Sanyi Tóth <sandor.toth@hotmail.com>
2021-03-06 12:13:11 +0100

    fetchPost() and addPost() created

--- OOP/blog.js
+++ OOP/blog.js
@@ -1,12 +1,14 @@
+import {Post} from "./post";
+
 export class Blog {
     #title;
     #author   // instanceof User Class
-    #posts = [];  // every item instanceof Posts Class
+    #posts;  // every item instanceof Posts Class
 
     constructor(title, author, posts) {
         this.#title = title;
         this.#author = author;
-        this.#posts = this.#posts.push(posts)
+        this.#posts = this.fetchPost()
     }
 
 
@@ -22,15 +24,28 @@ export class Blog {
         return this.#posts;
     }
 
-    #fetchPost() {
-
+    fetchPost() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                if (!response.ok) throw Error(response.statusText);
+                else return response.json();
+            })
+            .then(data => {
+                return data;
+            })
+            .catch(error => console.log(error))
     }
 
     addPost(postItem) {
-        
+        if (postItem instanceof Post) {
+            this.#posts.push(postItem)
+        } else throw Error(`${postItem} not an instance of that class`)
     }
 
-    deletePost() {
+    deletePost(postId) {
+
+
+
         //return boolean
     }
 
--- OOP/main.js
+++ OOP/main.js
@@ -1,3 +1,4 @@
 import {User} from './user.js';
 import {Post} from './post.js';
 import {Blog} from './blog.js';
+
