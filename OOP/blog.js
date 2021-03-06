546c9c70ff0055c7d2c2c795ff6dcb73fc7922a9
Sanyi Tóth <sandor.toth@hotmail.com>
2021-03-06 11:50:31 +0100

    blog class property and getters created

--- OOP/blog.js
+++ OOP/blog.js
@@ -1,4 +1,42 @@
 export class Blog {
+    #title;
+    #author   // instanceof User Class
+    #posts = [];  // every item instanceof Posts Class
+
+    constructor(title, author, posts) {
+        this.#title = title;
+        this.#author = author;
+        this.#posts = this.#posts.push(posts)
+    }
+
+
+    get title() {
+        return this.#title;
+    }
+
+    get author() {
+        return this.#author;
+    }
+
+    get posts() {
+        return this.#posts;
+    }
+
+    #fetchPost() {
+
+    }
+
+    addPost(postItem) {
+        
+    }
+
+    deletePost() {
+        //return boolean
+    }
+
+    getPostById(postId) {
+        //return Post class
+    }
 
 
 }
